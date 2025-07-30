84401444b38f4c9a79e8db6a2a0c434462e7e7a4
Pranav Suryavanshi <147048740+Pranav6185@users.noreply.github.com>
2025-07-30 09:57:32 +0530

    Update LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -231,19 +231,19 @@ const LandingPage = () => {
         {[
           {
             mode: "Online",
-            date: "21 july",
+            date: "1 August",
             time: "Hindi | 6:00 PM – 9:00 PM IST",
             link: "https://docs.google.com/forms/d/e/1FAIpQLSfxVYIstqh-TuQKcE4JUYJm8eBqTXLgftN1fQYN8MNRuqlN3w/viewform?usp=header",
           },
           {
             mode: "Offline",
-            date: "21 July",
+            date: "1 August",
             time: "Hindi | 11:00 AM – 1:00 PM IST",
             link: "https://docs.google.com/forms/d/e/1FAIpQLSfxVYIstqh-TuQKcE4JUYJm8eBqTXLgftN1fQYN8MNRuqlN3w/viewform?usp=header",
           },
           {
             mode: "Offline",
-            date: "21 July",
+            date: "1 August",
             time: "Marathi | 11:00 AM – 1:00 PM IST",
             link: "https://docs.google.com/forms/d/e/1FAIpQLSfxVYIstqh-TuQKcE4JUYJm8eBqTXLgftN1fQYN8MNRuqlN3w/viewform?usp=header",
           },
